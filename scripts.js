6eaab5cb8c2dc4689f34a109549a87682af8096f
OmarJ <omarjandali93@gmail.com>
2021-09-28 10:30:31 -0700

    tested all of the tip calculator functionality

--- scripts.js
+++ scripts.js
@@ -59,25 +59,3 @@ function decrementPeople(e){
         update();
     }
 }
-// const bill_input = document.querySelector('.bill');
-// const percentage_input = document.querySelector('.percentage');
-// const people_input = document.querySelector('.people');
-
-// const tip_btn = document.querySelector('#tip-btn');
-// const total_btn = document.querySelector('#total-btn')
-
-// tip_btn.addEventListener('click', calc_tip);
-
-// function calc_tip () {
-//  let bill = parseFloat(bill_input.value);
-//  let percentage = parseInt(percentage_input.value) / 100;
-//  let people = parseInt(people_input.value);
-//  let tip = (bill * percentage) / people;
-//  let billtotal = bill + (bill * percentage)
- 
-//  let tip_calc = document.querySelector('.tip-section .tip');
-//  tip_calc.innerText = '$' + tip.toFixed(2) + ((people > 1) ? ' per person' : '');
-//  //Different format for loop found on Stacks Overflow
-
-//  let total = document.getElementById('totality');
-//  total.innerHTML = '$' + billtotal.toFixed(2)
