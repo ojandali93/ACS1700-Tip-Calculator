c11524b6dd59489168da5ef0464e597d95319519
OmarJ <omarjandali93@gmail.com>
2021-09-28 10:03:43 -0700

    added the initial calculatons for tip and amount

---
+++ scripts.js
@@ -0,0 +1,67 @@
+const billInput = document.getElementById('bill-input')
+
+const tipIncrement = document.getElementById('tip-increment')
+const tipInput = document.getElementById('tip-selector')
+const tipDecrement = document.getElementById('tip-decrement')
+
+const peopleIncrement = document.getElementById('people-increment')
+const peopleInput = document.getElementById('people-selector')
+const peopleDecrement = document.getElementById('people-decrement')
+
+const tipOutput = document.getElementById('tip-output')
+const totalOutput = document.getElementById('total-output')
+
+billInput.addEventListener('input', update)
+
+tipIncrement.addEventListener('click', incrementTip)
+tipInput.addEventListener('input', update)
+tipDecrement.addEventListener('click', decrementTip)
+
+peopleIncrement.addEventListener('click', update)
+peopleInput.addEventListener('input', update)
+peopleDecrement.addEventListener('click', update)
+
+function update(e){
+    let bill = parseFloat(billInput.value)
+    let percentTip = parseFloat(tipInput.value) / 100
+    let people = parseInt(peopleInput.value)
+    let billTotal = bill + (bill * percentTip)
+    let tip = (bill * percentTip) / people
+    
+    tipOutput.innerHTML = tip.toFixed(2)
+    totalOutput.innerHTML = billTotal.toFixed(2)
+}
+
+function incrementTip(e){
+    let currentTip = parseFloat(tipInput.value) + 1;
+    tipInput.value = currentTip++;
+    update();
+}
+
+function decrementTip(e){
+    let currentTip = parseFloat(tipInput.value) - 1;
+    tipInput.value = currentTip++;
+    update();
+}
+// const bill_input = document.querySelector('.bill');
+// const percentage_input = document.querySelector('.percentage');
+// const people_input = document.querySelector('.people');
+
+// const tip_btn = document.querySelector('#tip-btn');
+// const total_btn = document.querySelector('#total-btn')
+
+// tip_btn.addEventListener('click', calc_tip);
+
+// function calc_tip () {
+//  let bill = parseFloat(bill_input.value);
+//  let percentage = parseInt(percentage_input.value) / 100;
+//  let people = parseInt(people_input.value);
+//  let tip = (bill * percentage) / people;
+//  let billtotal = bill + (bill * percentage)
+ 
+//  let tip_calc = document.querySelector('.tip-section .tip');
+//  tip_calc.innerText = '$' + tip.toFixed(2) + ((people > 1) ? ' per person' : '');
+//  //Different format for loop found on Stacks Overflow
+
+//  let total = document.getElementById('totality');
+//  total.innerHTML = '$' + billtotal.toFixed(2)
